0ef9d2a9a785662136b86e6f3d8c055d4be214e2
laoning <835742268@qq.com>
2012-04-08 13:01:32 +0800

    3

--- lining/example.c
+++ lining/example.c
@@ -2,9 +2,9 @@
 main()
 {printf("**********nice to meet you********");
 printf("hello,world!");
-<<<<<<< HEAD
-printf("**********I'm li ning********")
-=======
-printf("**********I'm gqf********")
->>>>>>> 48b1f9645e8ea97844bc83223f28eb082f1cd0b4
+
+printf("**********I'm li ning********");
+
+printf("**********I'm gqf********");
+
 }
--- lining/hello.c
+++ lining/hello.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 =======
 ﻿#include<stdio.h>
->>>>>>> 48b1f9645e8ea97844bc83223f28eb082f1cd0b4
+
 int main()
 {
         printf("Hello, World!\n");
@@ -11,10 +11,10 @@ int main()
         printf("您好，来自周红杰的问候\n");
 		printf("您好，我叫徐明华 ");
            printf("您好，我是李宁\n");
-<<<<<<< HEAD
-=======
+
+
         printf("您好，我是周红杰\n");
  printf("您好，我是龚泉枫\n");
->>>>>>> 48b1f9645e8ea97844bc83223f28eb082f1cd0b4
+
          
 }
