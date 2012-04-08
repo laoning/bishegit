cab92d8ff9b0ebcf492059f9e5e4054777c81d52
laoning <835742268@qq.com>
2012-04-08 16:20:30 +0800

    my folder

--- lining/example.c
+++ lining/example.c
@@ -10,35 +10,9 @@ printf("**********I'm gqf********");
 
 
 
-printf("**********nice to meet you********");
-printf("hello,world!");
-
-printf("**********I'm li ning********");
-
-printf("**********I'm gqf********");
-
-
-printf("**********nice to meet you********");
-printf("hello,world!");
-
-printf("**********I'm li ning********");
-
-printf("**********I'm gqf********");
 
 
 
-printf("**********nice to meet you********");
-printf("hello,world!");
-
-printf("**********I'm li ning********");
-
-printf("**********I'm gqf********");
 
 
-printf("**********nice to meet you********");
-printf("hello,world!");
-
-printf("**********I'm li ning********");
-
-printf("**********I'm gqf********");
 }
